8d9376658bb5923acd6de25f673b45df59ebc777
Christos Panagiotou <chpanag@gmail.com>
2025-11-18 13:56:30 +0200

    Fix import order with eslint auto-fix
    - Apply correct import sorting per perfectionist/sort-imports rule

--- src/sections/product/view/product-view.jsx
+++ src/sections/product/view/product-view.jsx
@@ -3,16 +3,16 @@ import Stack from '@mui/material/Stack';
 import { BackToTopButton } from 'src/components/animate/back-to-top-button';
 import { ScrollProgress, useScrollProgress } from 'src/components/animate/scroll-progress';
 
-import { ProductArchitecture } from '../product-architecture';
-import { ProductCapabilities } from '../product-capabilities';
-import { ProductContact } from '../product-contact';
-import { ProductFooter } from '../product-footer';
 import { ProductHero } from '../product-hero';
-import { ProductHowItWorks } from '../product-how-it-works';
+import { ProductFooter } from '../product-footer';
+import { ProductContact } from '../product-contact';
 import { ProductProblem } from '../product-problem';
 import { ProductSecurity } from '../product-security';
 import { ProductSolution } from '../product-solution';
 import { ProductUseCases } from '../product-use-cases';
+import { ProductHowItWorks } from '../product-how-it-works';
+import { ProductArchitecture } from '../product-architecture';
+import { ProductCapabilities } from '../product-capabilities';
 
 // ----------------------------------------------------------------------
 
